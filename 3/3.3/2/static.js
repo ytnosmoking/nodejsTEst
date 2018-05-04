791407026f21e1d444943dd40a96d8d73a6161b4
龚宇田 <gyt1@efanghang.com>
2018-05-04 17:38:24 +0800

    jade 模板使用 文件上传中文名decode res.render

--- 3/3.3/2/static.js
+++ 3/3.3/2/static.js
@@ -1,20 +1,18 @@
-// let BASE_DIR = __dirname;
-// let CONF = BASE_DIR + '/conf/';
-// let STATIC = BASE_DIR + '/static';
-// let mmieConf;
 
-// let sys = require('url'),
-//     http = require('http'),
-//     fs = require('fs'),
-//     url = require('url'),
-//     path = require('path');
-// mmieConf = getUrlConf();
 let http = require('http'),
+    jade = require('jade'),
     url = require('url');
 fs = require('fs');
 let staticModule = require('./static_module'); // 静态资源模块
-let upload = require('./upload').upload
+let upload = require('./upload').upload // 上传图片模块
 http.createServer((req, res) => {
+    res.render = function (template, options) {
+        let str = fs.readFileSync(template, 'utf8')
+        let fn = jade.compile(str, {filename: template,pretty: true});
+        let page = fn(options)
+        res.writeHead(200, { "Content-Type" : 'text/html'})
+        res.end(page)
+    }
     let pathname = url.parse(req.url).pathname;
     // console.log(pathname +'---19')
     if (pathname === '/favicon.ico') {
@@ -26,6 +24,9 @@ http.createServer((req, res) => {
     } else {
         staticModule.getStaticFile(pathname, res, req);
     }
+
+    
+
 }).listen(1337);
 
 
@@ -41,7 +42,10 @@ http.createServer((req, res) => {
  * @param {*} res 
  */
 function goIndex(res) {
-    let readPath = __dirname + '/' + url.parse('index.html').pathname;
-    let indexPage = fs.readFileSync(readPath);
-    res.end(indexPage);
+    // let readPath = __dirname + '/' + url.parse('index.html').pathname;
+    // let indexPage = fs.readFileSync(readPath);
+    // res.end(indexPage);
+    res.render(__dirname+'/jade/index.jade', {"user":"123"})
 }
+
+
--- 3/3.3/2/upload.js
+++ 3/3.3/2/upload.js
@@ -2,16 +2,25 @@
 let url = require('url')
 let formidable = require('formidable')
 exports.upload=(req, res) => {
-  let readPath = __dirname +'/'+ url.parse('show_image.html').pathname
-  let indexPage = fs.readFileSync(readPath)
+
+
+  var timestamp = Date.parse(new Date())
+
+  // let readPath = __dirname +'/'+ url.parse('show_image.html').pathname
+  // let indexPage = fs.readFileSync(readPath)
   let form = new formidable.IncomingForm()
   form.uploadDir = __dirname+'/uploadFile'
   form.parse(req, (err, fields, files) => {
     // console.log(fields)
+    let fileName = timestamp + '_' +files.image.name
     console.log(files.image.path)
     if(err) throw err;
-    fs.renameSync(files.image.path, __dirname+'/uploadFile/test.png');
-    res.writeHead(200, { "Content-type": 'text/html'})
-    res.end(indexPage)
+    fs.renameSync(files.image.path, __dirname+'/uploadFile/'+fileName);
+    // res.writeHead(200, { "Content-type": 'text/html'})
+    // res.end(indexPage)
+    res.render(__dirname+'/jade/show_image.jade', {
+      "imgUrl": "/uploadFile/"+fileName,
+      "showImg": " show up Image"+fileName
+    })
   })
 }
